41f7b230a96ca0f076379e61d078fa6aee5a3422
Tharindu1527 <tharindubandara15270@gmail.com>
2025-05-28 22:58:50 +0530

    Add post requests

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() 
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/")  # Decorator (we can specify Url)
+def root():  # Function
+    return {"message": "Hello World.."}
+
+@app.get("/posts")  #get - give me data
+def get_posts():
+    return {"posts": "This is your posts"}
+
+@app.post("/createposts")  #post - here is some data
+def create_posts():
+    return {"message": "successfully created posts"}
